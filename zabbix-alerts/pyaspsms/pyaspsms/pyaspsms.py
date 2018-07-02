8b34656b1ef823bcd7000ce7843d5749ed8cd8ba
Sergio Tocalini Joerg <sergiotocalini@gmail.com>
2018-07-02 13:56:24 +0200

    Adding the packages pyaspsms.

--- zabbix-alerts/pyaspsms/pyaspsms/pyaspsms.py
+++ zabbix-alerts/pyaspsms/pyaspsms/pyaspsms.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-import json, requests
+import yaml, json, requests
 
 class ASPSMS():
     options = { "api": "https://json.aspsms.com/" }
     def __init__(self, **kwargs):
         self.options.update(kwargs)
 
-   def SendTextSMS(self, msg, dest, **kwargs):
+   def SendTextSMS(self, **kwargs):
        self.options.update(kwargs)
        requires = [
            "UserName", "Password", "Recipients", "MessageText"
@@ -17,12 +17,25 @@ class ASPSMS():
            "URLNonDeliveryNotification", "AffiliateID", "ForceGSM7bit"
        ]
        data = {opt:self.options[opt] for opt in self.options if opt in requires + optional}
-      if not all(map(lambda x: data.get(x, '') != '', requires)):
-         return False
+       if not all(map(lambda x: data.get(x, '') != '', requires)):
+           return False
+       content = requests.post("%(api)s/SendTextSMS" %(self.options), data=json.dumps(data))
+       return content
 
-      content = requests.post("%(api)s/SendTextSMS" %(self.options), data=json.dumps(data))
-      return content
-
+   def SendSimpleTextSMS(self, **kwargs):
+       self.options.update(kwargs)
+       requires = [
+           "UserName", "Password", "Recipients", "MessageText"
+       ]
+       optional = [
+           "Originator", "ForceGSM7bit"
+       ]
+       data = {opt:self.options[opt] for opt in self.options if opt in requires + optional}
+       if not all(map(lambda x: data.get(x, '') != '', requires)):
+           return False
+       content = requests.post("%(api)s/SendSimpleTextSMS" %(self.options), data=json.dumps(data))
+       return content
+  
    def CheckCredits(self, **kwargs):
        self.options.update(kwargs)
        requires = ["Username", "Password"]
@@ -30,78 +43,57 @@ class ASPSMS():
        data = {opt:self.options[opt] for opt in self.options if opt in requires + optional}
        if not all(map(lambda x: data.get(x, '') != '', requires)):
            return False
-
        content = requests.post("%(api)s/CheckCredits" %(self.options), data=json.dumps(data))
        return content
 
-# def run(options):
-#    if options['daemon'] == True:
-#       ival = options.get('interval', 300)
-#       while True:
-#          myip = IPgetter().myip()
-#          for i in options['accounts']:
-#             logger.info('Trying with account: %s' %i)
-#             newopt = options.copy()
-#             newopt.pop('accounts')
-#             newopt.update(**options['accounts'][i])
-#             ddns_update(ip=myip, **newopt)
-#          logger.info(
-#             """Sleeping %s seconds to attempt to update again."""
-#             %ival)
-#          time.sleep(ival)
-#    else:
-#       myip = IPgetter().myip()
-#       for i in options['accounts']:
-#          newopt = options.copy()
-#          newopt.pop('accounts')
-#          newopt.update(**options['accounts'][i])
-#          ddns_update(ip=myip, **newopt)
-
-# def main():
-#    from optparse import OptionParser
-#    parser = OptionParser()
-#    parser.add_option("-c", "--config", dest="config",
-#                      help="Configuration file.", metavar="CONFIG")
-#    parser.add_option("-d", "--daemon", dest="daemon", default=False,
-#                      action="store_true",
-#                      help="Run as a daemon.")
-#    parser.add_option("-i", "--interval", dest="interval", default=300,
-#                      help="Configuration file", metavar="INTERVAL")
-#    parser.add_option("-t", "--token", dest="token",
-#                      help="Configuration file", metavar="TOKEN")
-#    parser.add_option("-w", "--webservice", dest="ws",
-#                      help="Webservice to commit", metavar="WEBSERVICE")
-#    parser.add_option("-q", "--quiet", dest="verbose", default=True,
-#                      action="store_false",
-#                      help="Don't print status messages to stdout.")
-#    (options, args) = parser.parse_args()
-
-#    if options.config:
-#       with open(options.config, 'r') as ymlfile:
-#          cfg = yaml.load(ymlfile)
-#          cfg.setdefault('logfile', '/var/log/donuts-ddns.log')
-#          cfg.setdefault('loglevel', 'info')
-#          cfg.setdefault('logformat',
-#                         """%(asctime)s - %(name)s - %(levelname)s - %(message)s""")
-#          cfg.setdefault('interval', 300)
-#          cfg['daemon']  = options.daemon
-#          cfg['quiet']   = options.verbose
-#          handler = logging.FileHandler(cfg['logfile'])
-#          handler.setFormatter(logging.Formatter(cfg['logformat'],
-#                                                 "%Y-%m-%d %H:%M:%S"))
-#          logger.addHandler(handler)
-#          logger.setLevel(getattr(logging, cfg.get('loglevel').upper()))
-#    else:
-#       cfg = {'interval': options.interval,
-#              'daemon': options.daemon,
-#              'quiet': options.verbose,
-#              'accounts':
-#              {'dry':
-#               {'ws': options.ws,
-#                'tokens': options.tokens}
-#              }
-#       }
-#    run(cfg)
-
-# if __name__ == '__main__':
-#    main()
+def main():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-c", "--config", dest="config",
+                      help="Your ASPSMS configurations.",
+                      metavar="ConfigFile")
+    parser.add_option("-u", "--username", dest="username",
+                      help="Your ASPSMS Userkey.",
+                      metavar="UserName")
+    parser.add_option("-p", "--password", dest="password",
+                      help="Your ASPSMS Password.",
+                      metavar="Password")
+    parser.add_option("-o", "--originator", dest="originator",
+                      help="You can use a phone number or up to 11 Alphabetic characters.",
+                      metavar="Originator")
+    parser.add_option("-r", "--recipients", dest="recipients",
+                      help="Add Recipients as JSON Array.",
+                      metavar="Recipients")
+    parser.add_option("-m", "--message", dest="message",
+                      help="The message text property accepts UTF8 encoding.",
+                      metavar="MessageText")
+    parser.add_option("-g", "--gsm7bit", dest="gsm7bit", default=False,
+                      action="store_false",
+                      help="""
+                      Force sending GSM7bit characters only to avoid extra cost by acidentally 
+                      using Unicode Characters.
+                      """, 
+                      metavar="ForceGSM7bit")    
+    parser.add_option("-d", "--debug", dest="debug", default=False,
+                      action="store_false",
+                      help="Print status messages to stdout.",
+                      metavar="Debug")
+    (options, args) = parser.parse_args()
+    
+    if options.config:
+        with open(options.config, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+    else:
+        cfg = {
+            'UserName': options.username,
+            'Password': options.password,
+            'Originator': options.originator,
+            'Recipients': options.recipients,
+            'MessageText': options.message,
+            'ForceGSM7bit': options.gsm7bit
+        }
+    sender = ASPSMS()
+    sender.SendSimpleTextSMS(**cfg)
+   
+if __name__ == '__main__':
+    main()
